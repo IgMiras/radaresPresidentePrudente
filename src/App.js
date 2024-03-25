eb7f81e9cd2299f294efa487216454b2df30bffd
IgMiras <im.miras@unesp.br>
2024-03-24 22:11:04 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import { radars } from './radars';
+import { Icon } from 'leaflet';
+import MarkerClusterGroup from 'react-leaflet-cluster';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const electronicBumpIcon = new Icon({
+		iconUrl: require('./assets/bump.png'),
+		iconSize: [38, 38], // size of the icon
+	});
+
+	const trafficLightRadarIcon = new Icon({
+		iconUrl: require('./assets/traffic-lights.png'),
+		iconSize: [38, 38], // size of the icon
+	});
+
+	const fixedRadarIcon = new Icon({
+		iconUrl: require('./assets/speed-radar.png'),
+		iconSize: [38, 38], // size of the icon
+	});
+
+	return (
+		<div className='App'>
+			<MapContainer center={{ lat: -22.121265, lng: -51.3834 }} zoom={13}>
+				<TileLayer
+					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+					url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+				/>
+
+				<MarkerClusterGroup>
+					{radars.map((radar) => {
+						if (radar.popUp === 'Lombada Eletronica') {
+							return (
+								<Marker
+									position={radar.geocode}
+									icon={electronicBumpIcon}
+								>
+									<Popup>{radar.popUp}</Popup>
+								</Marker>
+							);
+						}
+						if (radar.popUp === 'Radar Semaforico') {
+							return (
+								<Marker
+									position={radar.geocode}
+									icon={trafficLightRadarIcon}
+								>
+									<Popup>{radar.popUp}</Popup>
+								</Marker>
+							);
+						}
+						if (radar.popUp === 'Radar Fixo') {
+							return (
+								<Marker
+									position={radar.geocode}
+									icon={fixedRadarIcon}
+								>
+									<Popup>{radar.popUp}</Popup>
+								</Marker>
+							);
+						}
+					})}
+				</MarkerClusterGroup>
+			</MapContainer>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/radars.js
@@ -0,0 +1,110 @@
+export const radars = [
+	{
+		geocode: { lat: -22.123442569742753, lng: -51.39286977862696 },
+		popUp: 'Lombada Eletronica',
+	},
+	{
+		geocode: { lat: -22.121113101772405, lng: -51.40324245961067 },
+		popUp: 'Lombada Eletronica',
+	},
+	{
+		geocode: { lat: -22.129214821837905, lng: -51.3919463641279 },
+		popUp: 'Radar Semaforico',
+	},
+	{
+		geocode: { lat: -22.11915628559603, lng: -51.39097779341559 },
+		popUp: 'Radar Semaforico',
+	},
+	{
+		geocode: { lat: -22.120446786028282, lng: -51.3993778451843 },
+		popUp: 'Radar Semaforico',
+	},
+	{
+		geocode: { lat: -22.137706938849842, lng: -51.39077490365197 },
+		popUp: 'Radar Semaforico',
+	},
+	{
+		geocode: { lat: -22.12430153155176, lng: -51.42343555241786 },
+		popUp: 'Radar Semaforico',
+	},
+	{
+		geocode: { lat: -22.118387724335914, lng: -51.40355766900097 },
+		popUp: 'Radar Semaforico',
+	},
+	{
+		geocode: { lat: -22.127120121693196, lng: -51.38519559223556 },
+		popUp: 'Radar Semaforico',
+	},
+	{
+		geocode: { lat: -22.136774039380587, lng: -51.395950660796814 },
+		popUp: 'Radar Semaforico',
+	},
+	{
+		geocode: { lat: -22.12499839143751, lng: -51.38972608598487 },
+		popUp: 'Radar Semaforico',
+	},
+	{
+		geocode: { lat: -22.12078286579156, lng: -51.387434580496084 },
+		popUp: 'Radar Semaforico',
+	},
+	{
+		geocode: { lat: -22.138883118855617, lng: -51.39730266541643 },
+		popUp: 'Radar Semaforico',
+	},
+	{
+		geocode: { lat: -22.105250027900787, lng: -51.4305378629977 },
+		popUp: 'Radar Semaforico',
+	},
+	{
+		geocode: { lat: -22.144365867035443, lng: -51.38872560451 },
+		popUp: 'Radar Semaforico',
+	},
+	{
+		geocode: { lat: -22.099452695687212, lng: -51.38746246778706 },
+		popUp: 'Radar Semaforico',
+	},
+	{
+		geocode: { lat: -22.135934924513787, lng: -51.39580349273903 },
+		popUp: 'Radar Semaforico',
+	},
+	{
+		geocode: { lat: -22.138566666166838, lng: -51.406474247343965 },
+		popUp: 'Radar Fixo',
+	},
+	{
+		geocode: { lat: -22.14974537047426, lng: -51.41982411315513 },
+		popUp: 'Radar Fixo',
+	},
+	{
+		geocode: { lat: -22.091028890868753, lng: -51.438891043457566 },
+		popUp: 'Radar Fixo',
+	},
+	{
+		geocode: { lat: -22.08077489148997, lng: -51.37517920684101 },
+		popUp: 'Radar Fixo',
+	},
+	{
+		geocode: { lat: -22.094021569071405, lng: -51.398823195514304 },
+		popUp: 'Radar Fixo',
+	},
+	{
+		geocode: { lat: -22.168113751490647, lng: -51.41340921127224 },
+		popUp: 'Radar Fixo',
+	},
+	{
+		geocode: { lat: -22.11712816719427, lng: -51.373816015284845 },
+		popUp: 'Radar Fixo',
+	},
+	{
+		geocode: { lat: -22.135031567757576, lng: -51.364403983343166 },
+		popUp: 'Radar Fixo',
+	},
+	{
+		geocode: { lat: -22.148585117575333, lng: -51.34794396384294 },
+		popUp: 'Radar Fixo',
+	},
+	{
+		geocode: { lat: -22.130276226249, lng: -51.431767938708724 },
+		popUp: 'Radar Fixo',
+	},
+];
